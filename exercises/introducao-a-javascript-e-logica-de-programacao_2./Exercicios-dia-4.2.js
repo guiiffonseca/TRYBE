871a783615fee1a372b8de15aa1122e81f4e5037
guiiffonseca <guii.ffonseca22@gmail.com>
2021-05-20 18:19:49 -0300

    Exercicio 2

--- exercises/introducao-a-javascript-e-logica-de-programacao_2./Exercicios-dia-4.2.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2./Exercicios-dia-4.2.js
@@ -3,3 +3,11 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //Exercício 1-Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
 
 console.log(numbers);
+
+//Exercício 2- Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+ 
+let soma=0;
+for(let index=0; index < numbers.length; index+=1){
+    soma+=numbers[index];
+}
+console.log(soma);
