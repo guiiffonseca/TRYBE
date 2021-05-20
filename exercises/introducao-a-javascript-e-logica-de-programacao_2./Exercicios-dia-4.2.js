b0ef84cc61ae91a2d3ea66cab9b257e44975829f
guiiffonseca <guii.ffonseca22@gmail.com>
2021-05-20 19:34:06 -0300

    Exercicio 8

--- exercises/introducao-a-javascript-e-logica-de-programacao_2./Exercicios-dia-4.2.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2./Exercicios-dia-4.2.js
@@ -63,3 +63,12 @@ for(let index=0; index<numbers.length; index+=1){
     }
 }
 console.log('O menor valor é '+ menorValor);
+
+//Exercício 8-  Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
+
+let numbArray=[];
+
+for(let index=0; index<=25; index+=1){
+    numbArray.push(index);
+}
+console.log(numbArray)
