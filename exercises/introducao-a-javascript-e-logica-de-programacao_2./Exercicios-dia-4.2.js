150e1aecab7f7538bed0d945f905301ea9e57ece
guiiffonseca <guii.ffonseca22@gmail.com>
2021-05-20 19:22:38 -0300

    Exercicio 7

--- exercises/introducao-a-javascript-e-logica-de-programacao_2./Exercicios-dia-4.2.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2./Exercicios-dia-4.2.js
@@ -54,4 +54,12 @@ for(let index=0; index<numbers.length; index+=1){
         console.log(impares+' valores ímpares');
     }
 
-    
+//Exercício 7- Utilizando for , descubra qual o menor valor contido no array e imprima-o
+let menorValor=numbers[0];
+
+for(let index=0; index<numbers.length; index+=1){
+    if(numbers[index]<menorValor){
+        menorValor=numbers[index]
+    }
+}
+console.log('O menor valor é '+ menorValor);
