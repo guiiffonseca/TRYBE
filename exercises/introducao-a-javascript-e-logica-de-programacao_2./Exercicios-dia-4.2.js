c3309d678667ea57a38839b2a65979766cd2c860
guiiffonseca <guii.ffonseca22@gmail.com>
2021-05-20 18:28:06 -0300

    Exercicio 4

--- exercises/introducao-a-javascript-e-logica-de-programacao_2./Exercicios-dia-4.2.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2./Exercicios-dia-4.2.js
@@ -5,9 +5,18 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 console.log(numbers);
 
 //Exercício 2- Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
- 
-let soma=0;
+ let soma=0;
+
 for(let index=0; index < numbers.length; index+=1){
     soma+=numbers[index];
 }
-console.log(soma);
+console.log('A soma é igual a ' +soma);
+
+//Exercício 3 Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+//A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.
+let media=0
+
+media=soma/numbers.length;
+console.log('a média é '+media);
+
+console.log(numbers.length)
