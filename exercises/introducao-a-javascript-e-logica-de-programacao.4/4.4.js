8354d334b9afe68feb8a8bf175152ca18fe8d3bd
guiiffonseca <guii.ffonseca22@gmail.com>
2021-05-24 18:25:34 -0300

    Exercicios 1 e 2

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao.4/4.4.js
@@ -0,0 +1,12 @@
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+  console.log('Bem vinda, '+info.personagem+'!');
+
+  //EX 2
+  info.recorrente='Sim';
+  console.log(info.recorrente);
