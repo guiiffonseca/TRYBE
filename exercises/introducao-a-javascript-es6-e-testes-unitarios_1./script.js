2f007fe228e2d0de53b36ab14bdd9f21ead1ae3c
guiiffonseca <guii.ffonseca22@gmail.com>
2021-06-17 18:32:06 -0300

    Exercicio 2: Templates Literals e Array.Sort

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_1./script.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_1./script.js
@@ -28,3 +28,16 @@ let testingScope = (escopo) => {
 }
 
 testingScope(true);
+
+/*Copie o código abaixo e faça uma função que retorne o array oddsAndEvens em ordem crescente.
+Utilize template literals para que a chamada console.log(oddsAndEvens); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
+Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort() . Spoiler: É possível realizar uma função que ordene qualquer array de números.*/
+
+const oddsAndEvensSort = (a, b) => {
+  return a-b
+}
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+const odds = oddsAndEvens.sort(oddsAndEvensSort);
+  
+
+console.log(`Os números são: ${odds}`);
