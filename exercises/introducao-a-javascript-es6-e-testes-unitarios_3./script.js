9607f66e94b96c7b4fea959744a4b4a9d823f203
guiiffonseca <guii.ffonseca22@gmail.com>
2021-06-21 18:41:02 -0300

    Exercicio 2

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_3./script.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_3./script.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 
 function sum(a, b) {
   if (typeof a !== 'number' || typeof b !== 'number') {
@@ -7,7 +7,6 @@ function sum(a, b) {
 
   return a + b;
 }
-
 //Teste se o retorno de sum(4, 5) é 9
 assert.strictEqual(sum(4,5),9);
 
@@ -15,6 +14,29 @@ assert.strictEqual(sum(4,5),9);
 assert.strictEqual(sum(0,0),0);
 
 //Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
-assert.strictEqual(sum(4,'5'),9)
-Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")
+assert.strictEqual(sum(4,'5'),9);
+
+//Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")
+assert.throws(() => {
+  sum(4, '5');
+}, /^Error: parameters must be numbers$/);*/
+
+//A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item caso ele exista no array
+const assert = require('assert');
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
+  }
+  return newArr;
+}
+
+// Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
+assert.deepStrictEqual(myRemove([1,2,3,4],3),[1,2,4]);
+
+// Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+assert.notDeepStrictEqual(myRemove([1,2,3,4],3),[1,2,3,4]);
 
