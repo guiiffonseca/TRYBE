516254c4fc511526cac8ef8cd375a42d2e13bc58
guiiffonseca <guii.ffonseca22@gmail.com>
2021-06-23 18:13:18 -0300

    Finalizando bloco

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_3./script.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_3./script.js
@@ -40,3 +40,23 @@ assert.deepStrictEqual(myRemove([1,2,3,4],3),[1,2,4]);
 // Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
 assert.notDeepStrictEqual(myRemove([1,2,3,4],3),[1,2,3,4]);
 
+// A função myRemoveWithoutCopy(arr, item) recebe um array arr e retorna o próprio array sem o elemento item caso ele exista no array
+const assert = require('assert');
+
+function myRemoveWithoutCopy(arr, item) {
+  for (let index = 0, len = arr.length; index < len; index += 1) {
+    if (arr[index] === item) {
+      arr.splice(index, 1);
+      index -= 1;
+      len -= 1;
+    }
+  }
+
+  return arr;
+}
+
+// Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) retorna o array esperado
+assert.strictEqual(myRemove)
+
+
+
