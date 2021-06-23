070efcec314b1a5233908f8c0eacf8d70896b6be
guiiffonseca <guii.ffonseca22@gmail.com>
2021-06-23 18:59:00 -0300

    Exercicio 2 dia 8.1 Loteria

--- exercises/higher-order-functions-do-javascript-es6_1./script.js
+++ exercises/higher-order-functions-do-javascript-es6_1./script.js
@@ -15,3 +15,13 @@ const newEmployees = (callback) => {
 };
 
 console.log(newEmployees(generateEmail));
+
+// Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. 
+//O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
+const checker = (myNumer,number) => myNumer === number
+const result = (myNumer,callback) => {
+ const number = Math.floor(Math.random()*5)+1;
+ return callback(myNumer,number) ? 'Parabéns você ganhou' : 'Tente Novamente'
+ 
+}
+console.log(result(3,checker));
